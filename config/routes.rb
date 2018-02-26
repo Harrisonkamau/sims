6b95bddc298a41ee650c9e35fc2b0bf484617859
Harrisonkamau <kamauharrison87@gmail.com>
2018-02-26 18:30:03 +0300

    Ensure only authenticated users gain full access to navbar items

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   devise_scope :user do
     authenticated :user do
       root 'welcome#index', as: :authenticated_root
+      get '/about-sims', to: 'welcome#index', as: 'about'
     end
 
     unauthenticated do
