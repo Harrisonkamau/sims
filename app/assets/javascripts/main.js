a4216725a499d17e3758cd795c3ce4d2b5b09584
Harrisonkamau <kamauharrison87@gmail.com>
2018-03-05 10:01:26 +0300

    Fix turbolinks bug - interference with Bootstrap tabs

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,11 +1,11 @@
-$(function() {
-  setTimeout(function(){
-  $('.flashes').hide('blind', {}, 500)
+$(document).on('ready turbolinks:load', function () {
+  setTimeout(function () {
+    $('.flashes').hide('blind', {}, 500)
   }, 4000);
-
   // Activate tabs
   $('.nav-tabs a').click(function (e) {
     e.preventDefault()
     $(this).tab('show')
   })
+
 });
