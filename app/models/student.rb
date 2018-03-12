52c39a9ecd48bfb39080fea23a274d84058945ef
Harrisonkamau <kamauharrison87@gmail.com>
2018-03-12 14:59:21 +0300

    Create helper variables forcourses & departments

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,11 @@
 class Student < ApplicationRecord
   validates :first_name, :last_name, :admission_number, presence: true
+
+  DEPARTMENTS = [
+    'Arts', 'Business', 'Economics', 'Computer Science', 'Medicine',
+    'Geography'
+  ]
+  COURSES = [
+    'Arts', 'Sociology', 'IT', 'Economics', 'Medicine'
+  ]
 end
